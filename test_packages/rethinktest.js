1709a9194c8e0f0010342bb172f37d7907c975e7
Drew Showalter <maelstroswim@gmail.com>
2015-10-13 14:04:25 +1100

    diction

--- test_packages/rethinktest.js
+++ test_packages/rethinktest.js
@@ -1,8 +1,12 @@
 var r = require('rethinkdb');
-r.connect({ host: 'localhost', port: 28015 }, function(err, conn) {
-  if(err) {throw err}
+
+var port = 28015;
+if (process.argv[2]) {port = process.argv[2]}
+
+r.connect({ host: 'localhost', port: port }, function(err, conn) {
+  if (err) {throw err}
   r.db('test').tableCreate('tv_shows').run(conn, function(err, res) {
-    if(err) {throw err}
+    if (err) {throw err}
     console.log(res);
     r.table('tv_shows').insert({ name: 'Star Trek TNG' }).run(conn, function(err, res)
     {
